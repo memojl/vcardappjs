c175af153b317ce0f7fefdfa05bf3eada1e144fc
memojl <memojl08@gmail.com>
2023-03-17 10:18:30 -0600

    Update js tema

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ Versión: 1.2.2 - Vite
 */
 //Files import for webpack
 //import "./public/assets/dashboard/xeria/assets/css/bootstrap.css";
-import "./public/assets/dashboard/xeria/assets/css/icons.min.css";
+//import "./public/assets/dashboard/xeria/assets/css/icons.min.css";
 //import "./public/assets/dashboard/xeria/assets/css/app.css";
-import './public/assets/bootstrap-5.0.2/css/bootstrap.min.css';
+//import './public/assets/bootstrap-5.0.2/css/bootstrap.min.css';
 //import './public/assets/css/dashboard.css'; //** 3 WARNINGS IN WEBPACK FOR BUILD -->ADD DOM */
 //import './public/assets/font-awesome-5.14.0/css/all.min.css';
-import './public/assets/css/style.css';
+//import './public/assets/css/style.css';
 import './public/assets/css/load.css';
 //import './public/assets/main.scss';
-import './public/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
+//import './public/assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 //import './public/assets/js/sweetalert2.all.min.js';
 //import './public/assets/pwa/sw.js'; //SERVICE WORKER
 import './public/assets/pwa/appCon.js';
