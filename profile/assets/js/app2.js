27b996f59986fef620612a2a2c45ef512dacd82a
memojl <memojl08@gmail.com>
2023-05-11 09:12:08 -0600

    Update profile

--- profile/assets/js/app2.js
+++ profile/assets/js/app2.js
@@ -1,32 +1,35 @@
 /* VARIABLES CONSTANTES*/
 console.log('/* VARIABLES CONSTANTES*/');
 var loc = window.location;
-console.log(loc);
 var fecha = new Date();
-console.log(fecha);
-const protocol = window.location.protocol;
-console.log('protocol='+protocol);
-const host = window.location.host;
-console.log('host='+host);
-const dominio = window.location.origin+'/';
-console.log('dominio='+dominio);
-const dominio1 = window.location.origin;
-console.log('dominio1='+dominio1);
-const path_url = window.location.pathname;
-console.log('path_url='+path_url);
+const {protocol, host, origin, pathname} = window.location;
+const dominio = origin+'/';
+const dominio1 = origin;
+const path_url = pathname;
 const URL = window.location.href;
+if(host == 'localhost'){
+console.log(loc);
+console.log(fecha);
+console.log('protocol='+protocol);
+console.log('host='+host);
+console.log('dominio='+dominio);
+console.log('dominio1='+dominio1);
+console.log('path_url='+path_url);
+console.log('path_url='+path_url);
 console.log('URL='+URL);
+}
 /* VARIABLES */
 console.log('/* VARIABLES */');
 var path_url1 = path_url.replace("/", "");
 //var path_root=(host=='localhost')?path_url1:'';
 var path_root=(host=='localhost')?'MisSitios/vcardappjs/':'';
-console.log('path_root='+path_root);
 var page_url = dominio+path_root;
-console.log('page_url='+page_url);
 var perfil = filename();
+if(host == 'localhost'){
+console.log('path_root='+path_root);
+console.log('page_url='+page_url);
 console.log(perfil);
-
+}
 /*FUNCIONES */
 function filename(){
   var rutaAbsoluta = self.location.href;
@@ -55,6 +58,17 @@ function accion(type){
     if(type=='acceso'){$("#acceso").fadeIn();}else{$("#acceso").hide();}
 }
 
+function menuFooter(){
+  const menu = document.querySelector("#footerbar");
+  if(menu){
+    menu.addEventListener('click',(e)=>{//console.log(e);
+      setTimeout(() => {
+        location.href=URL+'#td2';      
+      }, 200);
+    });
+  }
+}
+
 function ssl(){
   //const protocol = window.location.protocol;console.log("protocol=" + protocol);
   if(protocol=="http:"){window.location="https://"+host+"/"+path_root;}
@@ -67,6 +81,7 @@ function sslProfile(){
 
 function load(){
   accion('inicio');
+  menuFooter();
 }
 
 document.onload = load();
