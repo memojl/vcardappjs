f8bc32bc538213f28e28b9411ea7372a1eac3919
multiportal <multiportal@outlook.com>
2024-03-24 19:12:24 -0600

    Update Login Google

--- src/pages/app/app.js
+++ src/pages/app/app.js
@@ -1,12 +1,27 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
-import { db } from "../../app/services/firebase";
+import { auth, db, saveUser, saveData } from "../../app/services/firebase";
 import { variables } from "../../app/core/lib";
 import { consoleLocal } from "../../app/functions";
+
+import {loginApp} from '../../app/hooks/googleLogin';
+
 /* VARIABLES CONSTANTES*/
-const { host, dominio, path_url, hostDev } = variables;
+const { host, dominio, mod, ext } = variables;
 
 function app(){console.log('Cargando App...');
-    consoleLocal('db',db);
+    consoleLocal('log',db);
+    loginApp()
+    onAuthStateChanged(auth, async (user) => {
+        if (user) {consoleLocal('log',user);
+            try {
+                saveUser(user);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+
+        }
+    });
 }
 
 export const loadApp = () => {
