4588792851902fcd030a5d1d30d1a369d2104195
multiportal <multiportal@outlook.com>
2024-08-04 19:38:04 -0600

    Update Vcard

--- src/pages/vcard/funciones.js
+++ src/pages/vcard/funciones.js
@@ -1,71 +1,108 @@
+import { consoleLocal } from "../../app/functions";
+
 /*FUNCIONES */
-export function filename() {
-    var rutaAbsoluta = self.location.href;
-    var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
-    var rutaRelativa = rutaAbsoluta.substring(posicionUltimaBarra + "/".length,rutaAbsoluta.length);
-    return rutaRelativa;
-}
-  
-export function action(type) {
-    if (type == "viewcopy") {
-      $("#inputCopiar").select();
-      var res = document.execCommand("copy");
-      if (!res) {
-        console.log("> No copy to clipboard");
-      } else {
-        console.log("Texto Copiado");
-        document.getElementById("aviso").innerHTML =
-          '<div class="alert alert-success">Texto Copiado!</div>';
-      }
+export async function getRegister(x, url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  //const uid = "sXP6Y9Uje3dy3cXZrzyY4aBwVhF2";
+  let res = null;
+  for (const key in data) {
+    if (data[key].profile === x || data[key].ID === x) {
+      res = data[key];
     }
+  }
+  return res;
 }
 
-export function accion(type) { //console.log(type);
-    //const closed = document.querySelector("#close");
-    if(type!='inicio'){$("#close").show();}else{$("#close").hide();}
-    if(type=='inicio'){$("#inicio").fadeIn();}else{$("#inicio").hide();}
-    if(type=='contacto'){$("#contacto").fadeIn();}else{$("#contacto").hide();}
-    if(type=='ubicacion'){$("#ubicacion").fadeIn();}else{$("#ubicacion").hide();}
-    if(type=='compartir'){$("#compartir").fadeIn();}else{$("#compartir").hide();}
-    if(type=='guardar'){$("#guardar").fadeIn();}else{$("#guardar").hide();}
-    if(type=='acceso'){$("#acceso").fadeIn();}else{$("#acceso").hide();}
+//ACCIONES MENU
+export function accion(type) { //consoleLocal('',type);
+  if (type != 'inicio') { $("#close").show(); } else { $("#close").hide(); }
+  if (type == 'inicio') { $("#inicio").fadeIn(); } else { $("#inicio").hide(); }
+  if (type == 'contacto') { $("#contacto").fadeIn(); } else { $("#contacto").hide(); }
+  if (type == 'ubicacion') { $("#ubicacion").fadeIn(); } else { $("#ubicacion").hide(); }
+  if (type == 'compartir') { $("#compartir").fadeIn(); } else { $("#compartir").hide(); }
+  if (type == 'guardar') { $("#guardar").fadeIn(); } else { $("#guardar").hide(); }
+  if (type == 'acceso') { $("#acceso").fadeIn(); } else { $("#acceso").hide(); }
+}
+
+export function filename() {
+  var rutaAbsoluta = self.location.href;
+  var posicionUltimaBarra = rutaAbsoluta.lastIndexOf("/");
+  var rutaRelativa = rutaAbsoluta.substring(posicionUltimaBarra + "/".length, rutaAbsoluta.length);
+  return rutaRelativa;
+}
+
+export function actionCopy(type) {
+  if (type == "viewcopy") {
+    $("#inputCopiar").select();
+    var res = document.execCommand("copy");
+    if (!res) {
+      console.log("> No copy to clipboard");
+    } else {
+      console.log("Texto Copiado");
+      document.getElementById("aviso").innerHTML = '<div class="alert alert-success">Texto Copiado!</div>';
+    }
+  }
 }
 
 export function menuFooter() {
-    const menu = document.querySelector("#footerbar");
-    if (menu) {
-      menu.addEventListener("click", (e) => {
-        //console.log(e);
-        setTimeout(() => {
-          location.href = URL + "#td2";
-        }, 200);
-      });
-    }
+  const menu = document.querySelector("#footerbar");
+  if (menu) {
+    menu.addEventListener("click", (e) => {//consoleLocal('',e.target);
+      const btn = e.target.id.replace('btn','').replace('1','').toLowerCase(); //console.log(btn); 
+      if(btn && btn!=''){ accion(btn); }
+      //setTimeout(() => {location.href = URL + "#td2";}, 200);
+    });
+  }
 }
-  
+
 export function ssl() {
-    //const protocol = window.location.protocol;console.log("protocol=" + protocol);
-    if (protocol == "http:") {
-      window.location = "https://" + host + "/" + path_root;
-    }
+  //const protocol = window.location.protocol;console.log("protocol=" + protocol);
+  if (protocol == "http:") {
+    window.location = "https://" + host + "/" + path_root;
   }
-  
+}
+
 export function sslProfile() {
-    //const protocol = window.location.protocol;console.log("protocol=" + protocol);
-    if (protocol == "http:") {
-      window.location = URL;
-    }
+  //const protocol = window.location.protocol;console.log("protocol=" + protocol);
+  if (protocol == "http:") {
+    window.location = URL;
   }
-  
-export  async function getRegister(x,url){
-    const response = await fetch(url);
-    const data = await response.json();
-    //const uid = "sXP6Y9Uje3dy3cXZrzyY4aBwVhF2";
-    let res = null;
-    for (const key in data) {
-      if (data[key].profile === x || data[key].ID === x) {
-        res = data[key];
-      }
-    }
-    return res;
+}
+
+export const isValidUrl = urlString => {
+  try {
+    return Boolean(new URL(urlString));
   }
+  catch(e){
+    return false;
+  }
+}
+
+export function validImage(url,ele,type){
+  var image = new Image();
+  image.src = url;
+  image.addEventListener('load', () =>{
+    const msj = (type == 0) ? 'Logo' : 'Foto de perfil';console.log('Success: '+msj);
+    (type == 0) ? ele.src = url : ele.style.backgroundImage = `url('${url}')`;
+  });
+  image.addEventListener('error', () =>{
+    const msj = (type == 0) ? 'logo' : 'foto de perfil';
+    console.error('Error: Fallo carga de '+msj);
+  });
+}
+
+export function validImage2(url,tmp,ele,type){
+  //window.addEventListener("load", event => {
+    var image = new Image();
+    image.src = url;
+    image.addEventListener('load', () => {
+      const msj = (type == 0) ? 'Logo' : 'Foto de perfil';console.log('Success: '+msj);
+      (type == 0) ? ele.src = url : ele.style.backgroundImage = `url('${url}')`;
+    });
+    image.addEventListener('error', () => {
+      const msj = (type == 0) ? 'logo' : 'foto de perfil';
+      if(tmp && tmp!=''){validImage(tmp,ele,type)}else{console.error('Error: Fallo carga de '+msj);}
+    });
+  //});
+}
--- src/pages/vcard/vcard.js
+++ src/pages/vcard/vcard.js
@@ -1,58 +1,85 @@
 import './vcard.css';
 import { variables } from "../../app/core/lib";
-import { getRegister, accion } from "./funciones";
+import { getRegister, isValidUrl, validImage, validImage2, accion, menuFooter } from "./funciones";
 import { Api } from '../../app/core/const.env';
 
 /* VARIABLES CONSTANTES*/
-const { host, dominio, mod, ext, id } = variables();
+const { host, dominio, mod, ext, id, URL } = variables();
 
-async function pagVcard() { console.log(id);
-  const data = await getRegister(id,Api+'/vcard_vcard.json');
-  console.log('Usuario',data);
-  const {nombre,puesto,descripcion} = data;
-  const dataEmpresa = await getRegister(data.idemp,Api+'/vcard_vcard_empresas.json'); 
-  console.log('Empresa',dataEmpresa);
+async function pagVcard() { //console.log(id);
+  const dataUser = await getRegister(id, Api+'/vcard_vcard.json'); console.log('Usuario',dataUser);
+  if(dataUser == null){document.querySelector('#container').style.display = 'none';return;}
+  const { nombre, profile, puesto, descripcion, fb, tw, ins, lk, web, email, cell, tel_ofi, cover, idemp, visible, direc } = dataUser;
+  const dataEmpresa = await getRegister(idemp, Api+'/vcard_vcard_empresas.json'); console.log('Empresa',dataEmpresa);
+  const {bg_color, coverbg, empresa} = dataEmpresa;
   accion('inicio');
+  menuFooter();  
 
   //INFO
+  document.querySelector('#head').style.backgroundColor = bg_color;
+  const tmpLogo = (id == 'multiportal')?'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpprRNyywxrgFdZ7rLvnq-6jEgWCBq_4Js3A&s':null;
+  validImage2(dataEmpresa.cover,tmpLogo,document.querySelector('#logo'),0);//document.querySelector('#logo').src = dataEmpresa.cover;
+  const tmpPerfil = (id == 'multiportal')?'https://multiportal.webcindario.com/assets/img/Multiportal.logo2023.png':null;
+  validImage2(cover,tmpPerfil,document.querySelector('#fotoperfil'),1);//document.querySelector('#fotoperfil').style.backgroundImage = `url('${cover}')`;
+
   document.querySelector('#tit').innerHTML = nombre;
   document.querySelector('#puesto').innerHTML = puesto;
   document.querySelector('#des').innerHTML = descripcion;
+  const medios = document.querySelector('#medios');
+  const cellHTML = (cell)?`<a href="tel:${cell}"><i class="fa fa-phone"></i><span>Llamar</span></a>`:'';
+  const waHTML = (cell)?`<a target="_blank" href="https://wa.me/+52${cell}"><i class="fa fa-whatsapp"></i><span>WhatsApp</span></a>`:'';
+  const mailHTML = (email)?`<a href="mailto:${email}"><i class="fa fa-envelope-o"></i><span>E-mail</span></a>`:'';
+  const webHTML = (web)?`<a href="${web}" id="empresaweb">${empresa}</a>`:'';
+  medios.innerHTML = cellHTML + waHTML + mailHTML + webHTML;
   //RRSS
-  //const fb = 
-  const redes = `
-  <div id="medios">
-    <a href="" target="_blank" aria-label="Facebook" class="icon-rrss">
+  const redes = document.querySelector('#redes');
+  const fbHtml = (fb)?`
+  <a href="${fb}" target="_blank" aria-label="Facebook" class="icon-rrss">
     <i class="fa fa-facebook"></i>
     <span>Facebook</span>
-    </a>
-  </div>`;
-  
+  </a>`:'';
+  const twHtml = (tw)?`
+  <a href="${tw}" target="_blank" aria-label="Twitter" class="icon-rrss">
+    <i class="fa fa-twitter"></i>
+    <span>Twitter</span>
+  </a>`:'';
+  const lkHtml = (lk)?`
+  <a href="${lk}" target="_blank" aria-label="LinkedIn" class="icon-rrss">
+    <i class="fa fa-linkedin"></i>
+    <span>LinkedIn</span>
+  </a>`:'';
+  const insHtml = (ins)?`
+  <a href="${ins}" target="_blank" aria-label="Instagram" class="icon-rrss">
+    <i class="fa fa-instagram"></i>
+    <span>Instagram</span>
+  </a>`:'';
+  redes.innerHTML = `<div id="medios">${fbHtml} ${twHtml} ${lkHtml} ${insHtml}</div>`;
+
+  //CONTACTO
+  const acell = document.querySelector('#a-cell');
+  if(cell){acell.setAttribute("href", "tel:"+cell);}else{acell.style.display = 'none';}
+  const awa = document.querySelector('#a-wa');
+  if(cell){awa.setAttribute("href", "https://wa.me/+52"+cell);}else{awa.style.display = 'none';}
+  const asms = document.querySelector('#a-sms');
+  if(cell){asms.setAttribute("href", "sms:"+cell);}else{asms.style.display = 'none';}
+  const aemail = document.querySelector('#a-email');
+  if(email){aemail.setAttribute("href", "mailto:"+email);}else{aemail.style.display = 'none';}
+  const aweb = document.querySelector('#a-web');
+  if(web){aweb.setAttribute("href", +web);}else{aweb.style.display = 'none';}
+  //UBICACION
+  const ubi = document.querySelector('#ubi'); //console.log(direc);
+  ubi.innerHTML = (direc)?`<p style="font-size: 18px;"><b>Dirección:</b> ${direc}</p>
+  <div id="map"></div>`:`<p>No hay dirección disponible.</p>`;
+  //COMPARTIR
+  document.querySelector('#inputCopiar').value = URL; //console.log(URL);
+  //GUARDAR CARD
+  document.querySelector('#saveCard').href = `files/vcf/${profile}.vcf`;
+
   //MENU
   const btnCerrar = document.querySelector('#close');
   if(btnCerrar){//console.log(btnCerrar.id);
     btnCerrar.addEventListener('click',()=>{accion('inicio')});
   }
-  const btnContacto = document.querySelector('#btnContacto');
-  if(btnContacto){//console.log(btnContacto.id);
-    btnContacto.addEventListener('click',()=>{accion(btnContacto.name)});
-  }
-  const btnUbicacion = document.querySelector('#btnUbicacion');
-  if(btnUbicacion){//console.log(btnUbicacion.id);
-    btnUbicacion.addEventListener('click',()=>{accion(btnUbicacion.name)});
-  }
-  const btnCompartir = document.querySelector('#btnCompartir');
-  if(btnCompartir){//console.log(btnUbicacion.id);
-    btnCompartir.addEventListener('click',()=>{accion(btnCompartir.name)});
-  }
-  const btnGuardar = document.querySelector('#btnGuardar');
-  if(btnGuardar){//console.log(btnUbicacion.id);
-    btnGuardar.addEventListener('click',()=>{accion(btnGuardar.name)});
-  }
-  const btnAcceso = document.querySelector('#btnAcceso');
-  if(btnAcceso){//console.log(btnUbicacion.id);
-    btnAcceso.addEventListener('click',()=>{accion(btnAcceso.name)});
-  }    
 }
 
 export default function vcard() {
