c897d17a25ea13d6d4392d3676fa6197528c4d83
multiportal <multiportal@outlook.com>
2025-04-28 18:22:29 -0600

    Update: css en app

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -13,8 +13,7 @@ import { getDatabase, ref, set, onValue, child, get } from "https://www.gstatic.
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 //import { alertMessage, showMessage } from "../hooks/messages";
-console.log('Firebase SDK');
-
+//console.log('Firebase SDK',typeof URL,URL);
 // Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyDeX81H_K8AsV2KjQgEbwxte6yVdSYqFXk",
